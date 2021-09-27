9e93e5a268d4b7f12f48897ecaac908f2c4ec431
Keshraf <82109991+Keshraf@users.noreply.github.com>
2021-09-27 23:55:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ const form = document.querySelector('form');
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
 });
+
+/* SCROLL BUTTON */
+const scrollButton = document.querySelector(".up-button");
+scrollButton.style.visibility = "hidden";
+const viewHeight = window.innerHeight;
+const scrollCheck = window.addEventListener('scroll', function(){
+    let scrollPosition = window.scrollY;
+    if(scrollPosition<(viewHeight/2)){
+        scrollButton.style.visibility = "hidden";
+    }
+    else{
+        scrollButton.style.visibility = "visible";
+    }
+    /* const about = document.querySelector(".analysis");
+    console.log(about.getBoundingClientRect()); */
+})
+
+
+
+
